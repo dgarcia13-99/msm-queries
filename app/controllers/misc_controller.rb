5f95846cf49e6d0fb70de0e962756ffc1113e83e
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-27 19:02:26 +0000

    filled templates and created paths and controllers for each corresponding page

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,37 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+  
+  def directors
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def director_page
+    render({ :template => "misc_templates/director_page"})
+  end
+
+  def youngest_director
+    render({ :template => "misc_templates/youngest_director"})
+  end
+
+  def eldest_director
+    render({ :template => "misc_templates/eldest_director"})
+  end
+
+  def movies
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def movie_page
+    render({ :template => "misc_templates/movie_page"})
+  end
+
+  def actors
+    render({ :template => "misc_templates/actors"})
+  end
+
+  def actor_page
+    render({ :template => "misc_templates/actor_page"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "misc", :action => "directors" })
+  get("/directors/:director_id", { :controller => "misc", :action => "director_page" })
+  get("/directors/youngest", { :controller => "misc", :action => "youngest_director" })
+  get("/directors/eldest", { :controller => "misc", :action => "eldest_director" })
+  get("/movies", { :controller => "misc", :action => "movies" })
+  get("/movies/:movie_id", { :controller => "misc", :action => "movie_page" })
+  get("/actors", { :controller => "misc", :action => "actors" })
+  get("/actors/:actor_id", { :controller => "misc", :action => "actor_page" })
 end
